021205b092e7b752a54501174aad50fb26091421
Samir <daniil.barinov.1996@mail.ru>
2025-05-03 01:09:52 +0300

    Добавил код в ProductRepository. Осталось добавить в ProductViewModel, чтобы добавлять в избранное.

--- app/src/main/java/com/example/kr_kotlin/ui/sub_catalog/ProductRepository.kt
+++ app/src/main/java/com/example/kr_kotlin/ui/sub_catalog/ProductRepository.kt
@@ -1,10 +1,17 @@
 package com.example.kr_kotlin.ui.sub_catalog
 
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 
 class ProductRepository {
     private val firestore = FirebaseFirestore.getInstance()
+    val auth = FirebaseAuth.getInstance()
+    val currentUserId = auth.currentUser?.uid ?: throw IllegalStateException("User not signed in")
+    private val productCol = firestore.collection("products")
+    private val favCol = firestore.collection("users").document(currentUserId)
+        .collection("favorites")
 
     fun getProducts( callback: (List<Product>) -> Unit) {
         firestore.collection("products").get().addOnSuccessListener { result ->
@@ -14,5 +21,20 @@ class ProductRepository {
             .addOnFailureListener {
                 callback(emptyList())
             }
+    }
+
+    fun addToFavorites (product : Product, onComplete: () -> Unit){
+        favCol.document(product.id).set(product).addOnSuccessListener { onComplete }
+    }
+
+    fun removeFromFarovires (productId: String, onComplete: () -> Unit) {
+        favCol.document(productId).delete().addOnSuccessListener { onComplete }
+    }
+
+    fun getFavotites (onResult: (List<Product>) -> Unit) {
+        favCol.get().addOnSuccessListener { snap ->
+            onResult(snap.map { it.toObject(Product::class.java).copy(id = it.id) })
+        }
+            .addOnFailureListener { onResult(emptyList()) }
     }
 }
